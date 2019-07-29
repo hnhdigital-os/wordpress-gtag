faf1c5e0fd77c322212851347e461fff3c538d0d
Rocco Howard <rocco@hnh.digital>
2019-07-29 12:27:12 +0000

    Gtag call through html attributes

--- js/hnhdigital-gtag.js
+++ js/hnhdigital-gtag.js
@@ -15,12 +15,39 @@ jQuery(document).ready(function($)
 
   gtag('config', tag_id);
 
-  console.log('Google Tag Manager: ' + tag_id);
+  console.log('GTag: ' + tag_id);
 
-  $('.gtag-report').on('click', function() {
-    gtag('event', 'conversion', {
-      'send_to': tag_id + '/R5E2CJrh1aUBEJKvzfQC',
-      'event_callback': callback
+  $('[data-gtag-trigger]').each(function() {
+    $(this).on($(this).data('gtag-trigger'), function() {
+      var data = data_to_object($(this).data('gtag-event-data'));
+
+      if (typeof $(this).data('gtag-event-callback') === 'string') {
+        data['callback'] = function() {
+          eval($(this).data('gtag-event-callback'));
+        }
+      }
+
+      gtag_event($(this).data('gtag-type'), data);
     });
   });
+
+  function data_to_object(data) {
+    var result = {};
+
+    if (data === null || typeof data !== 'string') {
+      return result;
+    }
+
+    data = data.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        result[pair[0]] = pair[1];
+    }
+
+    return result;
+  }
 });
+
+function gtag_event(type, data) {
+  gtag('event', type, data);
+}
